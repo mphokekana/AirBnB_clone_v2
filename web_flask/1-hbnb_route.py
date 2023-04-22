1278851fc9c6b593ec858af7002da5d08a70664b
Mpho Kekana <mphokekana736@gmail.com>
2023-04-22 10:45:14 -0700

    Task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application and return routes"""
-
+'''A simple Flask web application.
+'''
 from flask import Flask
 
 
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """Return a string"""
+@app.route('/')
+def index():
+    '''The home page.'''
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route('/hbnb')
 def hbnb():
-    """Return a string"""
+    '''The hbnb page.'''
     return 'HBNB'
 
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')

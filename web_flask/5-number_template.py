e37c1cde4c06b37a09065b2daf563ca9d1037668
Mpho Kekana <mphokekana736@gmail.com>
2023-04-22 10:56:28 -0700

    Task

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,53 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application and display a HTML page"""
+'''A simple Flask web application.
+'''
+from flask import Flask, render_template
 
-from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
+'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
-def hello_world():
-    """Return a string"""
+def index():
+    '''The home page.'''
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
-def hello():
-    """Return a string"""
+def hbnb():
+    '''The hbnb page.'''
     return 'HBNB'
 
-@app.route('/c/<text>')
-def hello_c(text):
-    """Return string and a variable text"""
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
 
-@app.route('/python/')
+@app.route('/c/<text>')
+def c_page(text):
+    '''The c page.'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
 @app.route('/python/<text>')
-def hello_python(text='is cool'):
-    """Return a string and a variable text"""
-    text = text.replace('_', ' ')
-    return 'Pythin {}'.format(text)
+@app.route('/python', defaults={'text': 'is cool'})
+def python_page(text):
+    '''The python page.'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
 
 @app.route('/number/<int:n>')
-def number(n):
-    """Return an integer n"""
-    n = str(n)
+def number_page(n):
+    '''The number page.'''
     return '{} is a number'.format(n)
 
+
 @app.route('/number_template/<int:n>')
-def num_template(n):
-    """Return an integer n"""
-    n = str(n)
-    return render_template('5-number.html', n=n)
+def number_template(n):
+    '''The number_template page.'''
+    ctxt = {
+        'n': n
+    }
+    return render_template('5-number.html', **ctxt)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')

912bb6fde737b8a0001b4770bd1c8935b4450b47
Mpho Kekana <111887473+mphokekana@users.noreply.github.com>
2023-10-08 21:33:44 +0000

    executable files

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -44,4 +44,3 @@ def number_text(n):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -31,4 +31,3 @@ def flask_teardown(exc):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
-
